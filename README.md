206a8572741f9517ceaa9a15b28edac581f6e5e0
Adrian <abcaballero.py@gmail.com>
2024-09-03 21:30:48 -0700

    added test for database connection and instructions for it
    updated readme
    
    configured .env files for sensitive database information

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Horizon
+
+## Info
+
+download the flask and msql connector dependencies in the media-app directory after download by running :  
+pip install Flask  
+pip install mysql-connector-python
--- media-app/backend/app.py
+++ media-app/backend/app.py
@@ -3,6 +3,7 @@ import mysql.connector
 
 app = Flask(__name__)
 
+#connecting to the database
 def get_db_connection():
     connection = mysql.connector.connect(
         host='horizon-llc.cdcgg8uief43.us-east-1.rds.amazonaws.com',
@@ -11,3 +12,23 @@ def get_db_connection():
         database='horizon'
     )
     return connection
+
+# checking if it connected to the database correctly 
+# run the flask app to see by running "python app.py" in the backend folder and then
+# opening your browser and going to "http://localhost:5000/test-db"
+# "ctrl + C to end it in the terminal"
+@app.route('/test-db', methods=['GET'])
+def test_db():
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        db_name = cursor.fetchone()
+        cursor.close()
+        connection.close()
+        return f"connected to database named ' {db_name[0]} ' successfully "
+    except mysql.connector.Error as err:
+        return f"Error: {err}"
+    
+if __name__ == "__main__":
+    app.run(debug=True)
