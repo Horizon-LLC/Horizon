c705b738dc80f592172105aa6680983052231b75
Hikaru Goto <Hikaru8803@gmail.com>
2024-09-29 23:19:01 -0700

    added modal for posting message

---
+++ media-app/frontend/src/MainPage/Feed.js
@@ -0,0 +1,12 @@
+import {Button, Card, CardBody, CardHeader, Input, Spacer} from "@nextui-org/react";
+
+
+const Feed = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default Feed;
--- media-app/frontend/src/MainPage/HomePage.js
+++ media-app/frontend/src/MainPage/HomePage.js
@@ -1,28 +1,73 @@
-import './MainPage.css'
-import {Button, Card, CardBody, CardHeader, Input, Spacer} from "@nextui-org/react";
+import './MainPage.css';
+import React, { useState } from 'react';
+import { Button, Card, Spacer, Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, useDisclosure, Input, Textarea } from '@nextui-org/react';
 
 const HomePage = () => {
+    const {isOpen, onOpen, onOpenChange} = useDisclosure();
+
+    const [message, setMessage] = useState('');  
+    const maxChar = 2000;            
+    
+    const messageLengthCheck = (e) => {
+        if (e.target.value.length <= maxChar) {
+            setMessage(e.target.value);
+        }
+    };
+
     return (
         <div className='container'>
-            <Card className='card-container'>
+            <Card className='side-container'>
                 <Button color="primary" className="max-w-xs">
                     List
                 </Button>
             </Card>
             <Spacer x={5} />
-            <Card className='card-container'>
-                <Button color="primary" className="max-w-xs">
+            <Card className='center-container'>
+                <Button color="primary" className="max-w-xs" onClick={onOpen}>
                     Create Post
                 </Button>
             </Card>
             <Spacer x={5} />
-            <Card className='card-container'>
+            <Card className='side-container'>
                 <Button color="primary" className="max-w-xs">
                     User Page
                 </Button>
             </Card>
+
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+                <ModalHeader>
+                    <h2>Create a New Post</h2>
+                </ModalHeader>
+                <ModalBody>
+                    <Input label="Title" placeholder="Enter the title of your post" fullWidth />
+                        <Spacer y={2} />
+                        <Textarea 
+                        label="Message" 
+                        placeholder="Write your message here..." 
+                        fullWidth 
+                        value={message}
+                        onChange={messageLengthCheck}
+                        maxLength={maxChar}
+                        />
+                        <p className='charCountText' >{message.length} / {maxChar} characters</p>
+                </ModalBody>
+                <ModalFooter>
+                    <Button auto flat color="danger" onClick={onClose}>
+                        Cancel
+                    </Button>
+                    <Button auto color='primary' onClick={onClose}>
+                        Post
+                    </Button>
+                </ModalFooter>
+            </>
+        )}
+        </ModalContent>
+      </Modal>
         </div>
-    )
+    );
 };
 
 export default HomePage;
