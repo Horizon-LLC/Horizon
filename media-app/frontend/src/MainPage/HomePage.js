5f0c3ac12760fbc7d9413c8ba443972548586f0e
Adrian <abcaballero.py@gmail.com>
2024-10-04 05:55:00 -0700

    updated format date in homepage to be consistent with AWS

--- media-app/frontend/src/MainPage/HomePage.js
+++ media-app/frontend/src/MainPage/HomePage.js
@@ -22,16 +22,7 @@ const HomePage = ({loggedInUser}) => {
     };
 
     const formatDate = (date) => {
-        const options = {
-            year: 'numeric',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false
-        };
-        return new Date(date).toLocaleString(undefined, options);
+        return new Date(date).toISOString();  // Use ISO format for consistency
     };
 
     const createPost = async () => {
